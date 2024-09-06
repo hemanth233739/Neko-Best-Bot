a4663c5a2391ce7d7e272710a05cc85a3bef4d4e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-09-06 22:32:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ logging.basicConfig(
 )
   
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-SUPPORT = os.environ.get("SUPPORT", None)
-UPDATES = os.environ.get("UPDATES", None)
-BOT_USERNAME = os.environ.get("BOT_USERNAME", None) 
+API_ID = os.environ.get("API_ID", 28597728)
+API_HASH = os.environ.get("API_HASH", "64ff048a1bb3a2096b682d7915dfb6d8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7106418611:AAGV2rVhZeHCypTWNRA9W8uam8dA8sKZsBs")
+SUPPORT = os.environ.get("SUPPORT", "NandhaSupport")
+UPDATES = os.environ.get("UPDATES", "NandhaBots")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "NekosBestBot") 
 
 
 bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
